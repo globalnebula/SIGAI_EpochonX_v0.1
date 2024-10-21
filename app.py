4649860422be11e574b7a76fa1d48d73732ec0d5
globalnebula <kunalachintya@gmail.com>
2024-10-21 17:20:49 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from groq import Groq
+
+def interact_with_llm(user_query, system_prompt, client):
+    client = Groq()
+    completion = client.chat.completions.create(
+        model="llama-3.1-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": ""
+            }
+        ],
+        temperature=1,
+        max_tokens=1024,
+        top_p=1,
+        stream=True,
+        stop=None,
+    )
+
+    for chunk in completion:
+        print(chunk.choices[0].delta.content or "", end="")
